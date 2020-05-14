74b60805f5aec2b4583af9fc977fb5b7f5d8063f
vincent-vianto <vincentvianto@gmail.com>
2020-05-14 10:42:22 +0700

    ~~~

--- src/actioncreators/Home.js
+++ src/actioncreators/Home.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
-const user = JSON.parse(localStorage.getItem("user"));
-const token = user.token;
-console.log(user);
-console.log(token);
-
 export const add = (data) => {
   return (dispatch) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const token = user.token;
+    console.log(user);
+    console.log(token);
     axios
-      .post("https://eyedentity-socialmedia.herokuapp.com/post/create", data, {headers: { 'token': token }})
+      .post("https://eyedentity-socialmedia.herokuapp.com/post/create", data, {
+        headers: { "token": token },
+      })
       .then((response) => {
         dispatch({
           type: "POST_ADD",
