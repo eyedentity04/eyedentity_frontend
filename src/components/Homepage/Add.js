cb77b4ee31b93f261f41ca170563166dc9c40af9
vincent-vianto <vincentvianto@gmail.com>
2020-05-19 11:44:34 +0700

    tags err

--- src/components/Homepage/Add.js
+++ src/components/Homepage/Add.js
@@ -1,38 +1,39 @@
 import React from "react";
-import {Formik} from 'formik'
+import { Formik } from "formik";
 import "./Add.css";
 import { connect } from "react-redux";
 import { add } from "../../actioncreators/Home";
-import {Form} from 'react-bootstrap'
-
+import { Form } from "react-bootstrap";
+import Tag from "./Tag";
 
 const Add = (props) => {
-  
   const user = JSON.parse(localStorage.getItem("user"));
-  const id = user.id;  
-  
+  const id = user.id;
+
   return (
-      <Formik
-        initialValues={{
-          name : id,
-          description : '',
-          image : null,
-        }}
-        onSubmit ={(values)=>{
-          let formData = new FormData();
+    <Formik
+      initialValues={{
+        name: id,
+        description: "",
+        image: null,
+        tags: [],
+      }}
+      onSubmit={(values) => {
+        let formData = new FormData();
 
-          formData.append('name',values.name)
-          formData.append('description',values.description)
-          formData.append('image',values.image)
+        formData.append("name", values.name);
+        formData.append("description", values.description);
+        formData.append("image", values.image);
+        formData.append("tag", values.tags);
 
-          props.add(formData);
-        }}
-      >
-        {props => (
-          <Form onSubmit={props.handleSubmit}>
-            <div className="container">
-              <div className="form-group">
-                <textarea
+        props.add(formData);
+      }}
+    >
+      {(props) => (
+        <Form onSubmit={props.handleSubmit}>
+          <div className="container">
+            <div className="form-group">
+              <textarea
                 className="form-control mt-5"
                 id="description"
                 name="description"
@@ -42,29 +43,36 @@ const Add = (props) => {
                 placeholder="Type something...."
                 onChange={props.handleChange}
               />
-              <br/>
+              
+              <Tag
+                value={props.values.tags}
+                onChange={props.handleChange}
+              />
+
               <input
                 type="file"
                 className="form-control"
                 id="image"
                 name="image"
-                onChange={(event)=>{
-                  props.setFieldValue('image',event.currentTarget.files[0])
+                onChange={(event) => {
+                  props.setFieldValue("image", event.currentTarget.files[0]);
                 }}
               />
-              </div>
-              <button type="submit" className="btn btn-info" style={{backgroundColor : "#8D7B65"}}>
-                Submit
-              </button>
             </div>
-          </Form>
-        )}
-      </Formik>
-    )
-  }
+            <button
+              type="submit"
+              className="btn btn-info"
+              style={{ backgroundColor: "#8D7B65" }}
+            >
+              Submit
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 const mapDispatchToProps = { add: add };
 
 export default connect(null, mapDispatchToProps)(Add);
-
-
