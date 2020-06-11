c258f9f2d92867473064e49e0c6ed7a44197f668
sony-nurdianto <sonynurdianto445@gmail.com>
2020-06-12 00:21:17 +0700

    comment done

--- src/components/Homepage/CommentPost.js
+++ src/components/Homepage/CommentPost.js
@@ -9,6 +9,8 @@ const CommentPost = (props) => {
 
   const url = process.env.REACT_APP_API_URL;
 
+  console.log(data._id)
+  
   useEffect(() => {
     async function myComment() {
       const user = JSON.parse(localStorage.getItem("user"));
@@ -17,33 +19,37 @@ const CommentPost = (props) => {
         .get(`${url}/comment/find/${data._id}`, {
           headers: { token: token },
         })
+        .then((result) => {
+          console.log(result)
+          setComment(result.data)
+        })
         .catch((err) => {
-          window.alert("error", err);
+          window.alert("error", err); 
         });
-      setComment(result.data);
+     
      
     }
+    console.log(myComment())
     myComment();
-  }, [props.comments]);
+  }, [data]);
 
-  useEffect(() => {
-    if (props.comments) {
-      if (props.comments.targetPostId == data._id) {
-        async function myComment() {
-          const user = JSON.parse(localStorage.getItem("user"));
-          const token = user.token;
-          const result = await axios
-          .get(`${url}/comment/find/${data._id}`, {
-            headers: { token: token },
-          })
-          setComment(result.data);
-        }
-        myComment()
-      }
-    }
-  },[props.comments]);
+  // useEffect(() =>{
+  //   console.log(data._id)
+  //   const user = JSON.parse(localStorage.getItem("user"));
+  //   const token = user.token;
+  //   axios.get(`${url}/comment/find/${data._id}`,{
+  //     headers : {"token" : token}
+  //   })
+  //   .then((result) => {
+  //     console.log(result)
+  //     setComment(result.data)
+  //   })
+  // },[data])
+
+
 
   let getComment = comment.map((item, index) => {
+    console.log(item.userComment.name)
     return (
       <div key={index}>
           <ul  className="list-group list-group-flush">
--- src/components/Homepage/PostId.js
+++ src/components/Homepage/PostId.js
@@ -13,12 +13,19 @@ import Comment from "../Homepage/Comment";
 import CommentPost from "../Homepage/CommentPost";
 import { addcomment } from "../../actioncreators/comment";
 import Navbar from "../Navbar";
+import TagsInput from "react-tagsinput";
 
 const PostId = (props) => {
+
+
   const url = process.env.REACT_APP_API_URL;
 
   const { _id } = useParams();
   const [data, setData] = useState({});
+  const [post,setPost] = useState({})
+  const [tag,setTag] = useState([])
+  const [like,setlike] = useState({})
+  const [place,setPlace] = useState([])
 
   const [comments, setComments] = useState("");
 
@@ -28,11 +35,24 @@ const PostId = (props) => {
     axios
       .get(`${url}/post/show/${_id}`, { headers: { token: token } })
       .then((res) => {
-        const data = res.data;
-        console.log(data);
-        setData(data);
+        axios
+        .get(`${url}/like/like`, { headers: { token: token } })
+        .then((likes) => {
+          console.log(likes)
+          setlike( {        
+            likesCount : likes.data.find((like) => res.data._id == like.postId)
+            ? likes.data.find((like) => res.data._id == like.postId).likes
+            : 0,
+          })
+        })
+
+        console.log(res.data.tagPlace[0])
+        setPlace(res.data.tagPlace[0])
+        setData(res.data.name)
+        setPost(res.data)
+        setTag(res.data.tag)
       });
-  }, [_id]);
+  }, []);
 
   const addlike = (targetPostId) => {
     props.addLike(targetPostId);
@@ -45,6 +65,14 @@ const PostId = (props) => {
     setComments(newComment);
   };
 
+
+  console.log(like)
+  console.log(data)
+  console.log(post._id)
+  let show = tag.map((item) => {
+    return item.name
+  })
+  
   const btnLikeClassName = data.likedByMe ? "bg-secondary" : "";
   return (
     <div>
@@ -54,42 +82,38 @@ const PostId = (props) => {
           <div className="card-header">
             <div className="d-flex flex-row">
               <img
-                src={`${url}/${data.name}`}
+                src={`${url}/${data.image}`}
                 style={{ height: "50px", width: "50px", borderRadius: "50%" }}
               />
               <div className="d-inline-flex flex-column">
-                {/* <p className="lead font-weight-bold mb-0 ml-2">
-                  {data.name.name}
-                </p>  */}
-                {/* <p className=" text-muted mb-0 ml-2">
-                  {data.tagPlace[0].namePlace !== "undefined"
-                    ? data.tagPlace[0].namePlace
+                <p className="lead font-weight-bold mb-0 ml-2">
+                  {data.name}
+                </p> 
+                <p className=" text-muted mb-0 ml-2">
+                  {place.namePlace !== "undefined"
+                    ? place.namePlace
                     : null}
-                </p> */}
+                </p>
               </div>
               <p className="text-muted ml-auto">{dayjs(data.date).fromNow()}</p>
             </div>
           </div>
           <div class="card-body">
-             {/* <p className="card-text text-muted mb-0">
-              {data.tag.map((item, index) => (
-                <span className="mr-1" key={index}>
-                  @{item.name}
-                </span>
-              ))}
-            </p> */}
-            <p className="card-text">{data.description}</p>
-            <img className="card-img-top" src={`${url}/${data.image}`} alt="" />
+             <p className="card-text text-muted mb-0">
+              {show}
+            </p>
+            <p className="card-text">{post.description}</p>
+            <img className="card-img-top" src={`${url}/${post.image}`} alt="" />
 
             <button
               type="button"
               className={`btn text-light mt-3 ${btnLikeClassName}`}
               onClick={() => {
-                addlike(data._id);
+                addlike(post._id);
               }}
             >
               <FontAwesomeIcon icon={faThumbsUp} className="fa-1x mx-auto" />
-              &nbsp; Like {data.likesCount}
+              &nbsp; Like {like.likesCount}
             </button>
 
             <div className=" mt-2">
@@ -105,7 +129,7 @@ const PostId = (props) => {
 
 const mapDispatchToProps = {
   addLike: addLike,
-  addcomment,
+  addcomment : addcomment,
 };
 
 export default connect(null, mapDispatchToProps)(PostId);
