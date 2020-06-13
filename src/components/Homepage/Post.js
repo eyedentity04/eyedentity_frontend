41f389c63c0489ec72d8ba8b2aa730a2050665a5
Agnes Jocelyn <Agnesjocelyn95@gmail.com>
2020-06-13 20:44:45 +0700

    Hilangin alerts hilangin text dllnya

--- src/components/Homepage/Post.js
+++ src/components/Homepage/Post.js
@@ -69,14 +69,13 @@ const Post = (props) => {
               }}
             >
               <FontAwesomeIcon icon={faThumbsUp} className="fa-1x mx-auto" />
-              &nbsp; Like {item.likesCount}
+              &nbsp;  {item.likesCount}
             </button>
 
 
             <Link to={`/${item._id}`}  target="_blank" className="btn text-light mt-3">
-            <FontAwesomeIcon icon={faComment} className="fa-1x mx-auto" />
             <span className="comment">
-            View All Comments 
+            Show All Comments 
             </span>
               
               </Link>
